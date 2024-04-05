5c16b38adc4caef484cb04f21a74b5c3cb7e450d
ankitbansal015 <ankitbansal015@gmail.com>
2024-04-05 12:16:39 +0530

    changes

--- Dicee Challenge - Completed/index.js
+++ Dicee Challenge - Completed/index.js
@@ -1,5 +1,5 @@
 
-// var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
